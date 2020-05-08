bade02e568bf51819f1fa96f8b79b8d25d1c4ed6
Matt Collins <matt@mjolka.com>
2020-05-08 14:13:09 +1000

    Add POST endpoints

--- NoCorsHeadersOnException/Controllers/WeatherForecastController.cs
+++ NoCorsHeadersOnException/Controllers/WeatherForecastController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,11 +8,6 @@ namespace NoCorsHeadersOnException.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -23,17 +15,16 @@ namespace NoCorsHeadersOnException.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost]
+        public ActionResult Post()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok();
+        }
+
+        [HttpPost("Error")]
+        public ActionResult Error()
+        {
+            throw new InvalidOperationException();
         }
     }
 }
